12fe238fc130fd66bc99f1012d112561687f960a
BalajiBommidi <153344987+BalajiBommidi@users.noreply.github.com>
2025-07-30 08:25:21 +0530

    Create app.py
    Creating an app.py with Hello form Azure DevOps Pipeline.

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Azure DevOps Pipeline!"
+
+if __name__ = "__main__":
+    app.run(hsot='0.0.0.0', port=80)
